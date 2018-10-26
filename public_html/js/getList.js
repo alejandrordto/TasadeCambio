108b96505036aa8d7b7dbc0a04e6d627765d360e
unknown <2112107@labinfo.is.escuelaing.edu.co>
2018-10-26 13:41:58 -0500

    canhge

--- public_html/js/getList.js
+++ public_html/js/getList.js
@@ -3,14 +3,38 @@
                 var menu = document.getElementById("moneda1");
                 var menu2 = document.getElementById("moneda2");
                 var menu3 = document.getElementById("moneda3");
-                var lista = ["JPY", "USD", "COP", "EUR", "BRL", "CAD", "AUD", "CHF", "GBP"];
+                var url="https://openexchangerates.org/api/latest.json?app_id=5a2f8324b0c64f47b759a8686685b9c1"
+                var xmlHttp = new XMLHttpRequest();
+                xmlHttp.open("GET", url, false); // false for synchronous request
+                xmlHttp.send(null);
+                var lista =xmlHttp.responseText;
+                lista=lista.split("\n");
+                lista=lista.slice(6,lista.length-2);
                 var i;
                 for (i = 0; i < lista.length; i++) {
-                    menu.options[i] = new Option(lista[i]);
-                    menu2.options[i] = new Option(lista[i]);
-                    menu3.options[i] = new Option(lista[i]);
+                    var temp=lista[i].split(":");
+                    menu.options[i] = new Option(temp[0]);
+                    menu2.options[i] = new Option(temp[0]);
+                    menu3.options[i] = new Option(temp[0]);
                 }
                 return lista;
             }
-
-
+function getvalues()
+{
+    var menu = document.getElementById("moneda1");
+                var menu2 = document.getElementById("moneda2");
+                var menu3 = document.getElementById("moneda3");
+                var url="https://openexchangerates.org/api/latest.json?app_id=5a2f8324b0c64f47b759a8686685b9c1"
+                var xmlHttp = new XMLHttpRequest();
+                xmlHttp.open("GET", url, false); // false for synchronous request
+                xmlHttp.send(null);
+                var lista =xmlHttp.responseText;
+                lista=lista.split("\n");
+                lista=lista.slice(6,lista.length-2);
+                var i;
+                for (i = 0; i < lista.length; i++) {
+                    var temp=lista[i].split(":");
+                    lista[i]=temp;
+                }
+                return lista;
+}
