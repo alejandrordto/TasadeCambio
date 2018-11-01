0e229d7e066c7c3da21a6dbac75b4a114a12539a
unknown <2112107@labinfo.is.escuelaing.edu.co>
2018-11-01 12:56:55 -0500

    finalmente

--- public_html/js/tabla.js
+++ public_html/js/tabla.js
@@ -1,7 +1,47 @@
 function genera_tabla() {
+
     var moneda = document.getElementById("moneda3").value;
+    if (!exists(moneda)) {
+        var monedas = getList();
+        var tabla = document.getElementById("tabla");
+        var tblBody = document.createElement("tbody");
+        for (var k in monedas) {
+            var fila = document.createElement("tr");
+            for (var j = 0; j < 2; j++) {
+                if (k !== moneda) {
+                    var columna = document.createElement("td");
+                    var celda;
+                    try {
+                        if (j == 0) {
+                            celda = document.createTextNode(k);
+                        } else {
+                            celda = document.createTextNode(read(k, moneda, monedas));
+                        }
+                    } catch (e) {
+
+                        if (e instanceof TypeError) {
+
+                        }
+                    }
+
+                    columna.appendChild(celda);
+                    fila.appendChild(columna);
+                }
+            }
+            tblBody.appendChild(fila);
+        }
+        tabla.appendChild(tblBody);
+    }
+}
+function exists(moneda) {
+    var temp = document.getElementById("tabla");
+    if (temp.getElementsByTagName("tbody").length !== 0) {
+        removetag( moneda);
+    }
+    return (temp.getElementsByTagName("tbody").length !== 0);
+}
+function removetag(moneda) {
     var monedas = getList();
-    var tabla= document.getElementById("tabla");
     var tblBody = document.createElement("tbody");
     for (var k in monedas) {
         var fila = document.createElement("tr");
@@ -9,16 +49,16 @@ function genera_tabla() {
             if (k !== moneda) {
                 var columna = document.createElement("td");
                 var celda;
-                try{
+                try {
                     if (j == 0) {
                         celda = document.createTextNode(k);
                     } else {
-                        celda = document.createTextNode(read(k,moneda,monedas));
+                        celda = document.createTextNode(read(k, moneda, monedas));
                     }
-                } catch (e){
-                    
-                    if (e instanceof TypeError){
-                        
+                } catch (e) {
+
+                    if (e instanceof TypeError) {
+
                     }
                 }
 
@@ -27,47 +67,12 @@ function genera_tabla() {
             }
         }
         tblBody.appendChild(fila);
+        
+        
     }
-    tabla.appendChild(tblBody);
-    exists(moneda);
-}
-function exists(monda){
-    var temp= document.getElementsByTagName("tbody");
-    if (document.getElementsByTagName("tbody")){
-        removetag();
-    }
-}
-function removetag(moneda){
-     var moneda = document.getElementById("moneda3").value;
-    var monedas = getList();
-    var tabla= document.getElementById("tabla");
-     var tblBody = document.createElement("tbody");
-    for (var k in monedas) {
-        var fila = document.createElement("tr");
-        for (var j = 0; j < 2; j++) {
-            if (k !== moneda) {
-                var columna = document.createElement("td");
-                var celda;
-                try{
-                    if (j == 0) {
-                        celda = document.createTextNode(k);
-                    } else {
-                        celda = document.createTextNode(read(k,moneda,monedas));
-                    }
-                } catch (e){
-                    
-                    if (e instanceof TypeError){
-                        
-                    }
-                }
-
-                columna.appendChild(celda);
-                fila.appendChild(columna);
-            }
-        }
-        tblBody.appendChild(fila);
-    }
-    //node.parentNode.removeChild(node);
+    var tabla = document.getElementById("tabla");
+    var body=tabla.getElementsByTagName("tbody")[0];
+    body.parentNode.replaceChild(tblBody,body);
 }
 
 
